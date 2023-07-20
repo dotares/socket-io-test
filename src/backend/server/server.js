d1d97d7b26ea0e851bf6bf11005794ee5f1419cd
fibbbonachos <arish2k3@gmail.com>
2023-07-20 12:17:04 +0530

    connect express with react

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT;
+
+app.use("/", express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.listen(port, () => {
   console.log(`running on ${port}`);
