ee925a258120b3e3ac108c622a84bd5d0bef08f4
fibbbonachos <arish2k3@gmail.com>
2023-07-21 11:33:38 +0530

    add http server instance for socket io

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -1,10 +1,22 @@
 require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
+
 const app = express();
+const http = require("http");
+
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server();
+
 const port = process.env.PORT;
 
 app.use("/", express.static(path.join(__dirname, "../../frontend/dist")));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
 app.listen(port, () => {
   console.log(`running on ${port}`);
